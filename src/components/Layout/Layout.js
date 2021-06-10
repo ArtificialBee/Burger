8d3805def5302ffb615c7a5f0afbd0c56ef4e8e5
ArtificialBee <edvinsacic@outlook.com>
2021-06-10 18:57:15 +0200

    Added sidedrawer (responsive need update)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Auxilliary'
 import './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const Layout = (props) => {
     return (
         <Aux>
             <Toolbar />
+            <SideDrawer />
             <div>Toolbar, SideDrawer, Backdrop</div>
             <main className="Content">{props.children}</main> {/*Ovo radimo kako bismo ovu komponentu mogli iskoristiti kao 'omot' za ostale komponente*/}
         </Aux>
